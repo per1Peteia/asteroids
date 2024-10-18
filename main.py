f7d1f106a80026a965d380bff91d3f054541b440
per1Peteia <justusmanthey@googlemail.com>
2024-10-18 20:23:20 +0200

    modified main.py with additional print statements

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print("Screen width: ", SCREEN_WIDTH)
+    print("Screen height: ", SCREEN_HEIGHT)
+
+if __name__ == "__main__":
+    main()
