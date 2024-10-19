1949ad7f6cab3c6b9674209f6d86baa43e63d3b8
per1Peteia <justusmanthey@googlemail.com>
2024-10-19 13:07:31 +0200

    updated main.py with a game loop and window close functionality

--- main.py
+++ main.py
@@ -6,9 +6,21 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    
     print("Starting asteroids!")
-    print("Screen width: ", SCREEN_WIDTH)
-    print("Screen height: ", SCREEN_HEIGHT)
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # get a new GUI
+
+    while True:                                                     # create a game loop
+        for event in pygame.event.get():                            # makes window close button work
+            if event.type == pygame.QUIT:                           # by checking if player closed window
+                return    
+        screen.fill((0, 0 , 0))                                     # fill the screen with black color
+        pygame.display.flip()                                       # refresh the screen
+
 
 if __name__ == "__main__":
     main()
